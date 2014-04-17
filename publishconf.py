8e830f53f0a3e9fe8c79b8a3e032afcd7358a83d
Andrew Starr-Bochicchio <a.starr.b@gmail.com>
2014-04-16 23:41:34 -0400

    Add --debug to pelican opts. Building fails without it.

--- publishconf.py
+++ publishconf.py
@@ -14,12 +14,9 @@ SITEURL = 'http://blog.andrewsomething.com'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
-CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
+#CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
 
 DELETE_OUTPUT_DIRECTORY = True
-
-FEED_ALL_ATOM = 'feeds/all.atom.xml'
-CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
 
 # Following items are often useful when publishing
 DISQUS_SITENAME = 'andrewsomething'
